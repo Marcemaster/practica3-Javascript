89f88b5cfb6cc1bd56736c0a7ecbc20d8864938a
Marcelo Hornillos <marcehornillos@gmail.com>
2021-12-17 18:18:37 +0100

    commit inicial equipos

---
+++ grupos.js
@@ -0,0 +1,5 @@
+const groups = ['A','B','C','D','E','F','G','H',]
+
+
+// Hay que crear una clase para los grupos para que cada una contenga a 4 equipos
+
---
+++ teams.js
@@ -0,0 +1,42 @@
+const europeTeams = [
+  "Alemania",
+  "Bélgica",
+  "Croacia",
+  "Dinamarca",
+  "España",
+  "Francia",
+  "Holanda",
+  "Inglaterra",
+  "Noruega",
+  "Polonia",
+  "Rusia",
+  "Serbia",
+  "Suiza",
+];
+const americaTeams = [
+  "Argentina",
+  "Bolivia",
+  "Brasil",
+  "Canada",
+  "Chile",
+  "Ecuador",
+  "Estados Unidos",
+  "México",
+  "Uruguay",
+];
+const africaTeams = [
+    "Argelia", 
+    "Camerún", 
+    "Egipto", 
+    "Ghana", 
+    "Nigeria"
+];
+const asiaTeams = [
+    "Arabia Saudita", 
+    "Australia", 
+    "Japón", 
+    "Qatar", 
+    "Siria"
+];
+
+const teams = [...africaTeams, ...asiaTeams, ...europeTeams, ...americaTeams];
